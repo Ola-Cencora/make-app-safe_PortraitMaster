68b99b2010ccce00c2f17ed09cc71a297c7e869e
Ola <cencora.aleksandra@gmail.com>
2024-01-17 14:14:48 +0100

    Fix multiply votes on the same photo

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -1,5 +1,7 @@
 const Photo = require("../models/photo.model");
+const Voter = require("../models/Voter.model");
 const sanitize = require("mongo-sanitize");
+const requestIp = require("request-ip");
 
 /****** SUBMIT PHOTO ********/
 
@@ -8,8 +10,9 @@ exports.add = async (req, res) => {
     const { title, author, email } = req.fields;
     const file = req.files.file;
 
-    if (title && author && email && file) { // if fields are not empty...
-      
+    if (title && author && email && file) {
+      // if fields are not empty...
+
       const emailPattern = new RegExp(/^[a-zA-Z.]+@[a-zA-Z]+\.[a-zA-Z]+$/);
       const stringPattern = new RegExp(/^[a-zA-Z0-9.,! ]+$/);
 
@@ -70,9 +73,31 @@ exports.vote = async (req, res) => {
     const photoToUpdate = await Photo.findOne({ _id: req.params.id });
     if (!photoToUpdate) res.status(404).json({ message: "Not found" });
     else {
-      photoToUpdate.votes++;
-      photoToUpdate.save();
-      res.send({ message: "OK" });
+      const clientIp = requestIp.getClientIp(req);
+      const userVoted = await Voter.findOne({ user: clientIp }); // did that person already vote?
+
+      if (!userVoted) { // if not, let them vote
+        const newVoter = new Voter({ // and add new voter to db
+          user: clientIp,
+          votes: photoToUpdate._id,
+        });
+        newVoter.save();
+
+        photoToUpdate.votes++; // add their vote to selected photo
+        photoToUpdate.save();
+        res.send({ message: "OK" });
+      } else if (userVoted) { // but if they have already voted
+        if (!userVoted.votes.includes(photoToUpdate._id)) { // check if they try to vote again on the same photo
+          userVoted.votes.push(photoToUpdate._id); // if not let them vote
+          userVoted.save();
+
+          photoToUpdate.votes++;
+          photoToUpdate.save();
+          res.send({ message: "OK" });
+        } else { // but if they are cheating dont let them
+          res.status(500).json(err);
+        }
+      }
     }
   } catch (err) {
     res.status(500).json(err);
